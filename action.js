13154e2a9e56b03e9e098ed70284b6dc675b670d
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-03 11:41:00 +0100

    Modified  action.js

--- action.js
+++ action.js
@@ -1,26 +1,34 @@
 const products = document.getElementById("products");
 for (let index = 0; index < 6; index += 1) {
+
   const row = document.createElement("div");
   row.setAttribute("class", "row");
+
   const image = document.createElement("img");
   image.setAttribute("src", "images/$(index).jpg");
+
   const productText = document.createElement("div");
   productText.setAttribute("class", "product-text");
   productTextH5 = document.createElement("h5");
   productTextH5.innerHTML = "Sale";
   productText.appendChild(productTextH5);
+
+  const heartIcon = document.createElement("div");
+  heartIcon.setAttribute("class", "heart-icon");
+  productTextH5 = document.createElement("h5");
+  productTextH5.innerHTML = "Sale";
+  productText.appendChild(productTextH5);
+
   row.appendChild(image);
   row.appendChild(productText);
   row.appendChild(image);
   row.appendChild(image);
   row.appendChild(image);
+
   products.appendChild(row);
 }
 
-<div class="row">
-  <div class="product-text">
-    <h5>Sale</h5>
-  </div>
+
   <div class="heart-icon">
     <i class="bx bxs-heart-circle"></i>
   </div>
@@ -35,4 +43,3 @@ for (let index = 0; index < 6; index += 1) {
     <h4>Half Running Set</h4>
     <p>$99 - $129</p>
   </div>
-</div>;
