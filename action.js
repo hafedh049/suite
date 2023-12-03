f9fabc0d3036190398b32fab2fbeee7301a1f622
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-03 11:32:28 +0100

    Modified  action.js

--- action.js
+++ action.js
@@ -1,4 +1,27 @@
 const products = document.getElementById("products");
 for (let index = 0; index < 6; index += 1) {
+    const row = document.createElement("div");
+    row.setAttribute("class","row")
   products.appendChild();
 }
+
+<div class="row">
+                <img src="images/1.jpg" alt="">
+                <div class="product-text">
+                    <h5>Sale</h5>
+                </div>
+                <div class="heart-icon">
+                    <i class='bx bxs-heart-circle'></i>
+                </div>
+                <div class="rating">
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star-half'></i>
+                </div>
+                <div class="price">
+                    <h4>Half Running Set</h4>
+                    <p>$99 - $129</p>
+                </div>
+            </div>
