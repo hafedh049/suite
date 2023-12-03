e53e7d259115a4d4dd0a673eacce70489284ddcb
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-03 12:00:45 +0100

    Modified  action.js

--- action.js
+++ action.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", this.window.scrollYY);
+  header.classList.toggle("sticky", this.window.scrollY > 0);
 });
 
 function loadProducts() {
