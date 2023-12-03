4b1a1b4663380a824978ed9a09045cddccf6cd93
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-03 11:55:37 +0100

    Modified  action.js

--- action.js
+++ action.js
@@ -1,6 +1,6 @@
 function loadProducts() {
   const products = document.getElementById("products");
-  for (let index = 0; index < 6; index += 1) {
+  for (let index = 1; index <= 8; index += 1) {
     const row = document.createElement("div");
     row.setAttribute("class", "row");
 
