b15a10cab72ab2d9fa15a211898c4d8df7f418e2
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-03 11:50:43 +0100

    Modified  action.js

--- action.js
+++ action.js
@@ -42,7 +42,7 @@ for (let index = 0; index < 6; index += 1) {
   row.appendChild(productText);
   row.appendChild(heartIcon);
   row.appendChild(rating);
-  row.appendChild(image);
+  row.appendChild(price);
 
   products.appendChild(row);
 }
