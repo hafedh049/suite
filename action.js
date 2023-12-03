010a5a043692d03b4b2403bb23b527c088ac01a9
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-03 11:32:37 +0100

    Modified  action.js

--- action.js
+++ action.js
@@ -2,7 +2,7 @@ const products = document.getElementById("products");
 for (let index = 0; index < 6; index += 1) {
     const row = document.createElement("div");
     row.setAttribute("class","row");
-  products.appendChild();
+  products.appendChild(row);
 }
 
 <div class="row">
