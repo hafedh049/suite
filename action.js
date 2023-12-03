5d3d2536dc5fec795d594b37f00a08c9f70da793
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-03 11:36:14 +0100

    Modified  action.js

--- action.js
+++ action.js
@@ -1,32 +1,35 @@
 const products = document.getElementById("products");
 for (let index = 0; index < 6; index += 1) {
-    const row = document.createElement("div");
-    row.setAttribute("class","row");
-    const image = document.createElement("img");
-    image.setAttribute("src","images/$(index).jpg");
-    const productText =  document.createElement("div");
-    productText.setAttribute("class","product-text")
-    row.appendChild()
-    products.appendChild(row);
+  const row = document.createElement("div");
+  row.setAttribute("class", "row");
+  const image = document.createElement("img");
+  image.setAttribute("src", "images/$(index).jpg");
+  const productText = document.createElement("div");
+  productText.setAttribute("class", "product-text");
+  row.appendChild(image);
+  row.appendChild(image);
+  row.appendChild(image);
+  row.appendChild(image);
+  row.appendChild(image);
+  products.appendChild(row);
 }
 
 <div class="row">
-              <img src="images/1.jpg">
-                <div class="product-text">
-                    <h5>Sale</h5>
-                </div>
-                <div class="heart-icon">
-                    <i class='bx bxs-heart-circle'></i>
-                </div>
-                <div class="rating">
-                    <i class='bx bxs-star'></i>
-                    <i class='bx bxs-star'></i>
-                    <i class='bx bxs-star'></i>
-                    <i class='bx bxs-star'></i>
-                    <i class='bx bxs-star-half'></i>
-                </div>
-                <div class="price">
-                    <h4>Half Running Set</h4>
-                    <p>$99 - $129</p>
-                </div>
-            </div>
+  <div class="product-text">
+    <h5>Sale</h5>
+  </div>
+  <div class="heart-icon">
+    <i class="bx bxs-heart-circle"></i>
+  </div>
+  <div class="rating">
+    <i class="bx bxs-star"></i>
+    <i class="bx bxs-star"></i>
+    <i class="bx bxs-star"></i>
+    <i class="bx bxs-star"></i>
+    <i class="bx bxs-star-half"></i>
+  </div>
+  <div class="price">
+    <h4>Half Running Set</h4>
+    <p>$99 - $129</p>
+  </div>
+</div>;
